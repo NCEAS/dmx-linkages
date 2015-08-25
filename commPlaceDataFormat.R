42aab48025d54a0922dc8a504bb4683378b8a4e3
Rachael Blake <blake@nceas.ucsb.edu>
2015-08-25 14:21:16 -0700

    added PWS Pink salmon data

--- commPlaceDataFormat.R
+++ commPlaceDataFormat.R
@@ -4,12 +4,30 @@
 ## This will replace the spreadsheet started on Google Drive.
 ################################################################
 
-## load packages
-library(dplyr)
+## load packages (order matters)
 library(httr)
+library(plyr)
+library(dplyr)
+
 
 ## create empty data frame with Year column
 CoPlc=data.frame('Year'=c(1975:2015))
 
-### bring in data, format to annual estimates (2 col df with cols=year,spEstimate) and merge with CoPlc dataframe
-# CoPlc=merge(CoPlc,newData,all.x=T) ## rename new df CoPlc
+## Steps for adding data columns: 
+## 1) read in data
+## 2) format to annual estimates (2 col df with cols=year,spEstimate)
+## 3) merge with CoPlc dataframe:   CoPlc=merge(CoPlc,newData,all.x=T)  # rename new df CoPlc
+
+# PWS wild Pink Salmon SSB:
+URL_Pinks <- "https://drive.google.com/uc?export=download&id=0By1iaulIAI-uVEZya3VTVnE3Wk0"
+PinksGet <- GET(URL_Pinks)
+Pinks1 <- content(PinksGet, as='text')
+WPinks <- read.csv(file=textConnection(Pinks1),stringsAsFactors=F)
+head(WPinks)
+#
+CoPlc <- merge(CoPlc,WPinks,all.x=T) 
+
+
+
+
+
