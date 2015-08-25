d3af105654dc500f70c9d3cbf43e31039d93d8a0
Rachael Blake <blake@nceas.ucsb.edu>
2015-08-25 10:19:49 -0700

    edited format, added comments

--- commPlaceDataFormat.R
+++ commPlaceDataFormat.R
@@ -1,11 +1,15 @@
-## Communities of Place Data compilation
+################################################################
+## Communities of Place data compilation
 ## created 24 Aug 2015
-## Add data to 
+## This will replace the spreadsheet started on Google Drive.
+################################################################
 
+## load packages
 library(dplyr)
+library(httr)
 
-CoPlc=data.frame(
-  'year'=c(1975:2015))
+## create empty data frame with Year column
+CoPlc=data.frame('Year'=c(1975:2015))
 
 ### bring in data, format to annual estimates (2 col df with cols=year,spEstimate) and merge with CoPlc dataframe
 # CoPlc=merge(CoPlc,newData,all.x=T) ## rename new df CoPlc
