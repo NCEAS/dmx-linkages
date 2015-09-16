b771c038da6e6985e84e607abb1cee5699d38541
Rachael Blake <blake@nceas.ucsb.edu>
2015-09-15 18:08:28 -0700

    added Stellar Sea Lion data

--- commPracticeDataFormat.R
+++ commPracticeDataFormat.R
@@ -569,6 +569,7 @@ upanom_cols <- scan(textConnection(upanom_pre), skip=2, nlines=1, what=character
 upanom_cols <- c("Lat", "Long", upanom_cols[-1])# split position into lat and long 
 upanom_df <- read.csv(file=textConnection(upanom_pre), skip=4, stringsAsFactors=F, sep="", 
                    header=FALSE, col.names=upanom_cols, strip.white=TRUE)
+#
 upanom <- upanom_df %>% 
           rename(Year=YEAR) %>% # rename data columns   
           filter(Year %in% c(1975:2015)) %>% # selects years 1975 - 2015
@@ -580,6 +581,51 @@ upanom <- upanom_df %>%
 CoPrct <- merge(CoPrct,upanom,all.x=T)
 
 ###############################################################################################
+### Stellar Sea Lions: 
+URL_SSL <- "https://goa.nceas.ucsb.edu/goa/metacat?action=read&qformat=metacatui&sessionid=&docid=df35b.270.1"
+SSLGet <- GET(URL_SSL)
+SSL1 <- content(SSLGet, as='text')
+SSL_df <- read.csv(file=textConnection(SSL1),stringsAsFactors=FALSE)
+head(SSL_df)
+#
+# NOTE: This is just means of the count data in the Central GOA region.  
+SSL <- SSL_df %>%
+     #  filter(counttype %in% c(2,3)) %>% # selects count types 2 & 3, which are vertical photos, 
+       rename(Year=year) %>%               # and according to the metadata were the most accurate 
+       filter(region=="C GULF", Year %in% c(1975:2015)) %>% # selects years 1975 - 2015), selects central GOA counts
+       arrange(Year) %>%
+       group_by(Year) %>%
+       summarise(SSLnonPup_anul_mn=mean(adult.juvenileCount, na.rm=TRUE)) %>% # get annual means
+       ungroup() 
+
+#####
+# This is code for trying to use the count data to calculate interpolated regional abundances.  
+#  I can't get it to work!!!
+
+#install.packages("devtools")
+#devtools::install_github("NMML/agTrend")
+#library(agTrend)
+#
+#SSL_fit <- mcmc.aggregate(start=1975, end=2015, data=SSL, aggregation="region", 
+#                          abund.name="adult.juvenileCount", time.name="Year", 
+#                          site.name="sitename", burn=1000, iter=5000, 
+#                          keep.site.abund=TRUE, model.data=NULL)
+#####
+
+CoPrct <- merge(CoPrct,SSL,all.x=T)
+
+###############################################################################################
+
+
+
+
+
+
+
+
+
+
+
 
 
 
