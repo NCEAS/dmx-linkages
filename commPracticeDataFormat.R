bbf91bc61638cd159002d64beb4a0381422c2b55
Colette Ward <colette.l.ward@gmail.com>
2015-09-16 17:58:42 -0700

    added May Copepod and Euphausiid data

--- commPracticeDataFormat.R
+++ commPracticeDataFormat.R
@@ -633,6 +633,40 @@ Phy <- Phy_df %>%
 CoPrct <- merge(CoPrct,Phy,all.x=T)
 
 ###############################################################################################
+### Zooplankton (from Seward Line dataset)
+# for full zooplankton processing scripts see dmx-common repository
+#
+# Load output of May Small zooplankton processing script. Values are biomass (g WW / m3):
+URL_SZo <- "https://drive.google.com/uc?export=download&id=0B1XbkXxdfD7uUXhnd2c3REszVDQ"
+SZoGet <- GET(URL_SZo)
+SZo1 <- content(SZoGet, as='text')
+SZo_df <- read.csv(file=textConnection(SZo1),stringsAsFactors=FALSE)
+head(SZo_df)
+#
+# Load output of May Large zooplankton processing script. Values are biomass (g WW / m3):
+URL_LZo <- "https://drive.google.com/uc?export=download&id=0B1XbkXxdfD7uSHRtNWdCQVRUWUE"
+LZoGet <- GET(URL_LZo)
+LZo1 <- content(LZoGet, as='text')
+LZo_df <- read.csv(file=textConnection(LZo1),stringsAsFactors=FALSE)
+head(LZo_df)
+#
+MayZo = full_join(SZo_df, LZo_df, by = "Year") # merge Small & Large Zooplankton datasets
+View(MayZo)
+#
+MayCopepods <- MayZo %>%
+  mutate(MayCopepods = rowSums(MayZo[,2:35], na.rm=T)) %>% # sum all small & large copepods
+  select(Year, MayCopepods)
+head(MayCopepods)
+#
+CoPrct <- merge(CoPrct,MayCopepods,all.x=T)
+#
+MayEuphausiids <- MayZo %>%
+  select(Year, Euphausiids)
+head(MayEuphausiids)
+#
+CoPrct <- merge(CoPrct,MayEuphausiids,all.x=T)
+
+###############################################################################################
 
 
 
