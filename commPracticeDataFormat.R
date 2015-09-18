7b4b2a03eaf7c3e483906d83c0fe786b2fc32823
Colette Ward <colette.l.ward@gmail.com>
2015-09-17 17:46:43 -0700

    added Pink Shrimp data

--- commPracticeDataFormat.R
+++ commPracticeDataFormat.R
@@ -686,8 +686,18 @@ head(Chl_df)
 CoPrct <- merge(CoPrct,Chl_df,all.x=T)
 #
 ########################################################################################################
-
-
+# Shrimp biomass (units = ?)
+# from NOAA / ADFG Small mesh trawl survey
+# estimates sent to Stephai Zador from Dan Urban
+#
+URL_Shr <- "https://drive.google.com/uc?export=download&id=0B1XbkXxdfD7ucFliX1NOZHRMOWc"
+ShrGet <- GET(URL_Shr)
+Shr1 <- content(ShrGet, as='text')
+Shr_df <- read.csv(file=textConnection(Shr1),stringsAsFactors=FALSE)
+head(Shr_df)
+CoPrct <- merge(CoPrct,Shr_df,all.x=T)
+#
+########################################################################################################
 
 
 
