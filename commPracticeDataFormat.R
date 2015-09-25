e4e4d2277863656aea0102b22a2723d14df2b280
Rachael Blake <blake@nceas.ucsb.edu>
2015-09-25 10:27:03 -0700

    made seperate upwelling anomalies script

--- commPracticeDataFormat.R
+++ commPracticeDataFormat.R
@@ -155,25 +155,7 @@ CoPrct <- merge(CoPrct,npgo_annual,all.x=T)
 CoPrct <- merge(CoPrct,pdo_annual,all.x=T)
 
 ###############################################################################################
-### Upwelling Anomalies:
-URL_upanom <- "http://www.pfeg.noaa.gov/products/PFELData/upwell/monthly/upanoms.mon"
-upanom_raw <- html(URL_upanom)
-upanom_pre <- upanom_raw %>%
-              html_node("p") %>%
-              html_text()
-upanom_cols <- scan(textConnection(upanom_pre), skip=2, nlines=1, what=character())# Get header row
-upanom_cols <- c("Lat", "Long", upanom_cols[-1])# split position into lat and long
-upanom_df <- read.csv(file=textConnection(upanom_pre), skip=4, stringsAsFactors=F, sep="",
-                   header=FALSE, col.names=upanom_cols, strip.white=TRUE)
-#
-upanom <- upanom_df %>%
-          rename(Year=YEAR) %>% # rename data columns
-          filter(Year %in% c(1975:2015)) %>% # selects years 1975 - 2015
-          gather(Month, UpwelAnom,-Year,-Lat,-Long) %>% # reshapes data to be column-wise
-          group_by(Year) %>%
-          summarise(UpWelAnom_anul_mn=mean(UpwelAnom, na.rm = TRUE)) %>% # get annual means
-          ungroup()
-#
+
 CoPrct <- merge(CoPrct,upanom,all.x=T)
 
 ###############################################################################################
