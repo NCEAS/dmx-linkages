d713f192a8783a67ec0e994f930833413c7ddf93
Rachael Blake <blake@nceas.ucsb.edu>
2015-09-25 10:19:13 -0700

    made seperate NPGO script

--- commPracticeDataFormat.R
+++ commPracticeDataFormat.R
@@ -147,21 +147,7 @@ CoPrct <- merge(CoPrct,Wind_Winter,all.x=T)  # Winter mean wind
 CoPrct <- merge(CoPrct,ENSO_annual,all.x=T)
 
 ##############################################################################################
-# North Pacific Gyre Oscillation Index (NPGO):
-URL_npgo <- "http://www.o3d.org/npgo/npgo.php"
-npgo_pre <- xpathSApply(content(GET(URL_npgo)),"/html/body/pre", xmlValue)
-npgo_cols <- scan(textConnection(npgo_pre), skip=25, nlines=1, what=character())# Get header row
-npgo_cols <- npgo_cols[2:4] # select column names
-npgo_df <- read.csv(file=textConnection(npgo_pre), skip=26, stringsAsFactors=F, sep="",
-                 header=FALSE, col.names=npgo_cols, strip.white=TRUE)
 
-npgo_annual <- npgo_df %>%
-               rename(Year=YEAR) %>% # rename data columns
-               filter(Year %in% c(1975:2015)) %>% # selects years 1975 - 2015
-               group_by(Year) %>%
-               summarise(NPGO_anul_mn=mean(NPGO)) %>% # get annual means
-               ungroup()  #
-#
 CoPrct <- merge(CoPrct,npgo_annual,all.x=T)
 
 ##############################################################################################
