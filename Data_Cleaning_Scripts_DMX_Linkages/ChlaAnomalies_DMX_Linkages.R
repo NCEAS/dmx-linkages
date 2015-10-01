8f9b8c2a4b438adf24fcc9ee648b3bde3200ae29
Colette Ward <colette.l.ward@gmail.com>
2015-09-30 18:52:08 -0700

    updated Chl a dataset from anomalies to raw

--- Data_Cleaning_Scripts_DMX_Linkages/ChlaAnomalies_DMX_Linkages.R
+++ Data_Cleaning_Scripts_DMX_Linkages/ChlaAnomalies_DMX_Linkages.R
@@ -1,6 +1,6 @@
 ###########################################################
 ##### Data Cleaning Script - DMX Linkages
-##### Mean annual Chl a anomalies (mg/m3) for Gulf of Alaska
+##### Mean annual Chl a (mg/m3) for Gulf of Alaska
 ###########################################################
 
 ## load packages (order matters)
@@ -18,15 +18,25 @@ library(stringr)
 ## 2) format to annual estimates (2 column dataframe with cols=Year,spEstimate)
 
 #############
-# Mean annual Chl a anomalies (mg/m3) for Gulf of Alaska
-# From Waite & Mueter 2013, Fig 11 Annual
-# Waite, J.N. and Mueter, F.J. 2013. Spatial and temporal variability of chlorophyll-a concentrations
+
+# Data from Waite, J.N. and Mueter, F.J. 2013. Spatial and temporal variability of chlorophyll-a concentrations
 # in the coastal Gulf of Alaska, 1998-2011, using cloud-free reconstructions of SeaWiFS and MODIS-Aqua data.
 # Prog. Oceanogr. 116, 179-192.
+# Data sent by Jason Waite Sept 25 2015
 
-URL_Chl <- "https://drive.google.com/uc?export=download&id=0B1XbkXxdfD7uRHdOTGQtSVBQOE0"
+# data are log mean annual Chl a (mg/m3), calculated from 8-day mean log chl-a levels for weeks 7-38
+# regions: se = southeast, woff = western offshelf, won = western onshelf, cen = central
+# NB this processing script is in dmx-common repository
+
+URL_Chl <- "https://drive.google.com/uc?export=download&id=0B1XbkXxdfD7uMjNnWnZGMnBoUFk"
 ChlGet <- GET(URL_Chl)
 Chl1 <- content(ChlGet, as='text')
 Chl_df <- read.csv(file=textConnection(Chl1),stringsAsFactors=FALSE)
+View(Chl_df)
+str(Chl_df)
 
-#
+# calculate means across western offshelf, western onshelf, and central regions:
+AnnChl = Chl_df %>%
+  mutate(AnnChl = rowMeans(Chl_df[,2:4])) %>%
+  select(year, AnnChl)
+View(AnnChl)
