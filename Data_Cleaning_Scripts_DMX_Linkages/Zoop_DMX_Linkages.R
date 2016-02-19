7a0b9644f878282c530014b90d5e0520707ea547
Colette Ward <colette.l.ward@gmail.com>
2016-02-19 15:47:15 +0100

    updated column names for Seward Line zooplankton

--- Data_Cleaning_Scripts_DMX_Linkages/Zoop_DMX_Linkages.R
+++ Data_Cleaning_Scripts_DMX_Linkages/Zoop_DMX_Linkages.R
@@ -28,22 +28,22 @@ SZoGet <- GET(URL_SZo)
 SZo1 <- content(SZoGet, as='text')
 SZo_df <- read.csv(file=textConnection(SZo1),stringsAsFactors=FALSE)
 head(SZo_df)
-#
+
 # Load output of May Large zooplankton processing script. Values are biomass (g WW / m3):
 URL_LZo <- "https://drive.google.com/uc?export=download&id=0B1XbkXxdfD7uSHRtNWdCQVRUWUE"
 LZoGet <- GET(URL_LZo)
 LZo1 <- content(LZoGet, as='text')
 LZo_df <- read.csv(file=textConnection(LZo1),stringsAsFactors=FALSE)
 head(LZo_df)
-#
+
 MayZo = full_join(SZo_df, LZo_df, by = "Year") # merge Small & Large Zooplankton datasets
-#
-MayCopepods <- MayZo %>%
-               mutate(MayCopepods = rowSums(MayZo[,2:35], na.rm=T)) %>% # sum all small & large copepods
-               select(Year, MayCopepods)
-#
-MayEuphausiids <- MayZo %>%
-                  select(Year, Euphausiids)
-#
+
+SewardLineMayCopepods <- MayZo %>%
+               mutate(SewardLineMayCopepods = rowSums(MayZo[,2:35], na.rm=T)) %>% # sum all small & large copepods
+               select(Year, SewardLineMayCopepods)
+
+SewardLineMayEuphausiids <- MayZo %>%
+                  rename(SewardLineMayEuphausiids = Euphausiids) %>%
+                  select(Year, SewardLineMayEuphausiids)
 
 
