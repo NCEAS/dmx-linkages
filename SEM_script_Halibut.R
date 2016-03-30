eecc0c56203ee4a9eb67ae5b85e178a7b5b35b03
Colette Ward <colette.l.ward@gmail.com>
2016-03-30 15:29:00 -0700

    added Halibut commercial quota to Halibut SEM - model fits still poor

--- SEM_script_Halibut.R
+++ SEM_script_Halibut.R
@@ -34,11 +34,11 @@ CPrHlbt <- CPrD %>%
          NPGO = NPGO_anul_mn,
          SharkAbund = SharkAbundIPHC) %>%
   select(Year, ENSO, NPGO, logCrab, logPlckAdults, logPinkShrimp, SharkAbund, 
-         logHlbt, logHlbtPounds, hlbt_real_rev, hlbt_vessels, hlbt_processors)
+         logHlbt, logHlbtPounds, hlbt_real_rev, hlbt_vessels, hlbt_processors, HlbtQuota_2C3A3B)
 names(CPrHlbt)
 
 # look at correlations among Halibut variables
-# pairs.panels(CPrHlbt[,c(2:12)],smooth=F,density=T,ellipses=F,lm=T,digits=3,scale=T)
+# pairs.panels(CPrHlbt[,c(2:13)],smooth=F,density=T,ellipses=F,lm=T,digits=3,scale=T)
 # Hlbt revenue, vessels, processors are correlated
 
 
@@ -65,33 +65,35 @@ CPrHlbtLags = CPrHlbt %>%
          logPinkShrimp.lag6to15 = rollapply(data = logPinkShrimp.lag6, width = 10, FUN = mean, 
                                             align = "right", fill = NA, na.rm = T), # find mean of logPinkShrimp over lags 6-15
          
-         # Cannot use shark data, time series begins in 1998, therefore there is insufficient data to create lagged time series
-         #Sharks.lag6 = lag(SharkAbund, n = 6),
-         #Sharks.lag6to15 = rollapply(data = Sharks.lag6, width = 10, FUN = mean, 
-                                     #align = "right", fill = NA, na.rm = T), # find mean of Shark Abundance over lags 6-15
+         Sharks.lag6 = lag(SharkAbund, n = 6),
+         Sharks.lag6to15 = rollapply(data = Sharks.lag6, width = 10, FUN = mean, 
+                                     align = "right", fill = NA, na.rm = T), # find mean of Shark Abundance over lags 6-15
   
          logHlbt.lead1 = lead(logHlbt, n = 1)) %>% # create autoregressive term (lag -1) for Halibut biomass
   
          select(Year, ENSO.lag7to16, NPGO.lag7to16, logCrab, logCrab.lag6to15, 
-                logPlckAdults, logPinkShrimp.lag6to15, #Sharks.lag6to15, 
-                logHlbt, logHlbt.lead1, logHlbtPounds, hlbt_real_rev, hlbt_vessels, hlbt_processors)
+                logPlckAdults, logPinkShrimp.lag6to15, Sharks.lag6to15, 
+                logHlbt, logHlbt.lead1, logHlbtPounds, hlbt_real_rev, hlbt_vessels, hlbt_processors, HlbtQuota_2C3A3B)
 
 # Important: data do not exist to calculate lagged variables before 1991 (ie first year for which lag 7to16 data can be used is 1991)
 
 
-
 # select 1996 - 2013
 CPrHlbtLags1 <- CPrHlbtLags %>%
-  filter(Year > 1995 & Year < 2014)
+  filter(Year > 1995 & Year < 2014 %>%
+  select(-Sharks.lag6to15) %>% # Cannot use shark data for 1996-2013 analyses because time series begins in 1998, therefore there is insufficient data to create lagged time series
+  select(-HlbtQuota_2C3A3B) # quota system began in 2006  
+
 
 
 # look at correlations
-pairs.panels(CPrHlbtLags1 [,c(2:13)],smooth=F,density=T,ellipses=F,lm=T,digits=3,scale=T)
+pairs.panels(CPrHlbtLags1 [,c(2:14)],smooth=F,density=T,ellipses=F,lm=T,digits=3,scale=T)
 # significant correlations (for variables in the same regressions within the model):
 # ENSO.lag7to16, NPGO.lag7to16
 # logPinkShrimp.lag6to15, logCrab.lag6to15
-# Hlbt revenue, vessels, processors
-
+# Hlbt vessels, processors
+# quota correlates with all of HlbtPounds, revenue, vessels, processors
+# quota correlates strongly with all variables ...
 
 # standardize each variable to zero mean and unit variance
 CPrHlbtLags2 <- CPrHlbtLags1 %>% colwise(scale)()
@@ -229,6 +231,7 @@ summary(mod.7.fit, stand=T, rsq=T)
 
 # still very poor model fit, even though p values are significant for all regressions ...
 # no meaningful improvement in min test statistic relative to mod.6
+# and shrimp term should be positive, not negative ... lagged shrimp and adult halibut are strongly negatively correlated
 
 # lavaan (0.5-20) converged normally after  44 iterations
 # Number of observations                            18
@@ -258,3 +261,75 @@ summary(mod.7.fit, stand=T, rsq=T)
 #                Estimate
 # logHlbtPounds     0.178
 # logHlbt           0.996
+
+
+
+
+# look at just the Adult Halibut component:
+summary(lm(CPrHlbtLags2$logHlbt ~ CPrHlbtLags2$logPinkShrimp.lag6to15 + CPrHlbtLags2$logHlbt.lead1))
+
+
+
+######################################################################
+######################################################################
+######################################################################
+
+# add Halibut Commercial Catch Limit to model
+# there was no quota system before 2006; therefore restrict model to 2006 - 2013
+
+
+# select 2006 - 2013
+CPrHlbtLags3 <- CPrHlbtLags %>%
+  filter(Year > 2005 & Year < 2014)
+         
+# standardize each variable to zero mean and unit variance
+CPrHlbtLags4 <- CPrHlbtLags3 %>% colwise(scale)()
+
+
+
+
+# Base model:
+mod.8 <- 'logHlbtPounds ~ logHlbt + HlbtQuota_2C3A3B
+
+HlbtQuota_2C3A3B ~ logHlbt.lead1                                         # Quota is set by standing biomass in year t-1
+
+logHlbt ~ logPinkShrimp.lag6to15 +                                       # variables influencing age-0 and age-1 Halibut
+logHlbt.lead1                                                            # autoregressive process
+
+'
+mod.8.fit <- sem(mod.8, data=CPrHlbtLags4)
+summary(mod.8.fit, stand=T, rsq=T)
+
+# model min test statistic is improved, however p-val is still not significant, despite highly significant p values and high R^2 for each regression parameter
+# suggests another link (pathway) is still missing
+# why is halibut harvest negatively related to halibut biomass?
+
+# lavaan (0.5-20) converged normally after  46 iterations
+# Number of observations                             8
+# Estimator                                         ML
+# Minimum Function Test Statistic               12.940
+# Degrees of freedom                                 4
+# P-value (Chi-square)                           0.012
+
+# Regressions:
+#                    Estimate  Std.Err  Z-value  P(>|z|)   Std.lv  Std.all
+# logHlbtPounds ~                                                         
+#   logHlbt            -1.123    0.113   -9.966    0.000   -1.123   -0.939
+#   HlbtQut_2C3A3B      1.939    0.113   17.207    0.000    1.939    1.622
+# HlbtQuota_2C3A3B ~                                                      
+#   logHlbt.lead1       0.854    0.184    4.637    0.000    0.854    0.854
+# logHlbt ~                                                               
+#   lgPnkShrmp.615     -0.463    0.130   -3.549    0.000   -0.463   -0.463
+#   logHlbt.lead1       0.546    0.130    4.192    0.000    0.546    0.546
+
+# Variances:
+#                Estimate  Std.Err  Z-value  P(>|z|)   Std.lv  Std.all
+# logHlbtPounds     0.027    0.014    2.000    0.046    0.027    0.022
+# HlbtQut_2C3A3B    0.237    0.119    2.000    0.046    0.237    0.271
+# logHlbt           0.017    0.009    2.000    0.046    0.017    0.020
+
+# R-Square:
+#                Estimate
+# logHlbtPounds     0.978
+# HlbtQut_2C3A3B    0.729
+# logHlbt           0.980
