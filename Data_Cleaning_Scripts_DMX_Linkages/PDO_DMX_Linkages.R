03bf6517a53f001867a54fda8264c5bada87137e
Colette Ward <colette.l.ward@gmail.com>
2016-08-22 10:20:18 +0200

    updated PDO script to calculate annual winter means

--- Data_Cleaning_Scripts_DMX_Linkages/PDO_DMX_Linkages.R
+++ Data_Cleaning_Scripts_DMX_Linkages/PDO_DMX_Linkages.R
@@ -29,13 +29,22 @@ pdo_df <- read.table(file=textConnection(pdo_pre), skip=30, nrows=116, stringsAs
                   header=FALSE, col.names=pdo_cols, strip.white=TRUE, fill=TRUE)
 pdo_df$YEAR <- substr(pdo_df$YEAR, 1, 4)  # removes asterisks from years 2002-2015
 
-pdo_annual <- pdo_df %>%
+pdo_pre2 <- pdo_df %>%
               rename(Year=YEAR) %>% # rename data columns
-              filter(Year %in% c(1975:2015)) %>% # selects years 1975 - 2015
+              filter(Year %in% c(1974:2015)) %>% # selects years 1974 - 2015
+              select(Year, JAN, FEB, DEC) %>% # select only Dec, Jan, Feb data
               gather(Month, PDO, -Year) %>% # reshapes data to be column-wise
-              group_by(Year) %>%
-              summarise(PDO_anul_mn=mean(as.numeric(as.character(PDO)), na.rm = TRUE)) %>% # get annual means
-              ungroup()
+              mutate(Year = as.integer(Year))
+  
+for(i in 1:nrow(pdo_pre2)) {
+  if(pdo_pre2$Month[i] == "DEC") {pdo_pre2$Year[i] <- pdo_pre2$Year[i]+1} # assign year t+1 to each Dec estimate, so that sequential Dec, Jan, Feb estimates are grouped
+}
+
+pdo_winter <- pdo_pre2 %>%
+  filter(Year != 1974, Year != 2016) %>% # remove year 1974 now that 1974 data for December has been assigned to the 1975 group
+  group_by(Year) %>%
+  summarise(PDO_winter = mean(as.numeric(as.character(PDO)), na.rm = TRUE)) %>% # get winter means
+  ungroup()
 #
 
 
